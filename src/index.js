7f76d6dc057f21c47450829f06e27416f38aea26
kindlingscript <inflamedmuse@gmail.com>
2017-02-27 03:22:18 -0600

    Added videos to starting page + CSS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 import API_KEY from './key';
 
 /****
 * Create a new component - it should produce some HTML.
 ****/
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        videos: [] ,
+        selectedVideo: null
+      };
+
+    YTSearch({key: API_KEY, term: 'scenery'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 /****
